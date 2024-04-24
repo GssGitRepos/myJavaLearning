c8a1d102405b562b9426bc15d86ae1c379c666e3
sarath <saisarath.g@appistoki.in>
2024-04-24 16:13:30 +0530

    added MethodParameters file

---
+++ src/MethodParameters.java
@@ -0,0 +1,44 @@
+class Robot {
+	String material = "Steel";
+	String joints = "hinge";
+
+	void speak() {
+		System.out.println("Hello There!");
+	}
+
+	void jump(int height) {
+		System.out.println("Jumping Height is:" + height + " metres");
+	}
+
+	void info(String name, int year) {
+		System.out.println("Hello World! My Name is " + name + " and I am made in the year " + year + " by Sarath");
+
+	}
+
+	String speed(int memory) {
+		String dialogue = "Speed is " + memory + " tera hertz";
+		return dialogue;
+	}
+}
+
+public class MethodParameters {
+	public static void main(String[] args) {
+
+		Robot chitti = new Robot();
+		chitti.speak();
+
+		// Passing parameter into the method
+
+		chitti.jump(10);
+
+		// passing multiple parameters into the method
+
+		chitti.info("chitti", 2024);
+
+		// passing parameter and printing the returned value
+
+		System.out.println(chitti.speed(2));
+
+	}
+
+}
